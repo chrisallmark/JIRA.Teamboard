50cbc3e2b7ec38f8ed0fb679e26a556fd27ba1ea
Chris Allmark <chris@allmarkconsulting.com>
2014-08-21 12:48:45 +0100

    Tweaked loader formatting

--- app/js/controllers/loaderController.js
+++ app/js/controllers/loaderController.js
@@ -11,14 +11,14 @@ angular.module('JIRA.Teamboard')
             'Welcome <strong>changing requirements</strong>, even late in  development.<br/>Agile processes harness change for the <strong>customer\'s competitive advantage</strong>.',
             'Deliver <strong>working software</strong> frequently, from a couple of weeks<br/>to a couple of months, with a preference to the <strong>shorter timescale</strong>.',
             'Business people and developers must<br/><strong>work together daily</strong> throughout the project.',
-            'Build projects around <strong>motivated individuals</strong>.<br/>Give them the <strong>environment and support</strong><br/>they need, and <strong>trust</strong> them to get the job done.',
-            'The most efficient and effective method of conveying information to<br/>and within a development team is <strong>face-to-face conversation</strong>.',
+            'Build projects around <strong>motivated individuals</strong>. Give them the<br/><strong>environment and support</strong> they need, and <strong>trust</strong> them to get the job done.',
+            'The most <strong>efficient and effective</strong> method of conveying information to<br/>and within a development team is <strong>face-to-face conversation</strong>.',
             '<strong>Working software</strong> is the<br/>primary measure of progress.',
             'Agile processes promote <strong>sustainable development</strong>.<br/>The sponsors, developers, and users should be<br/>able to <strong>maintain a constant pace</strong> indefinitely.',
             'Continuous attention to <strong>technical excellence</strong><br/>and <strong>good design</strong> enhances agility.',
-            'Simplicity —<br/>the art of <strong>maximizing the amount of work not done</strong><br/>— is essential.',
+            'Simplicity — the art of <strong>maximizing the<br/>amount of work not done</strong> — is essential.',
             'The best architectures, requirements, and designs<br/>emerge from <strong>self-organising teams</strong>.',
-            'At regular intervals, the team reflects<br/>on how tobecome <strong>more effective</strong> then,<br/><strong>tunes and adjusts</strong> it\'s behaviour accordingly.'
+            'At regular intervals, the team reflects on how to become<br/><strong>more effective</strong>, then <strong>tunes and adjusts</strong> it\'s behaviour accordingly.'
         ];
         $scope.loader = {};
         $scope.$watch('teamboard.loaded', function() {
