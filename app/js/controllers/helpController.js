4f26cd108d80c99724b7461e5cc62df80b35b41f
Chris Allmark <chris@allmarkconsulting.com>
2014-08-26 14:04:46 +0100

    Added GPL 3.0

--- app/js/controllers/helpController.js
+++ app/js/controllers/helpController.js
@@ -1,3 +1,21 @@
+/*
+JIRA Teamboard - an electronic information radiator
+Copyright (C) 2014 Chris Allmark
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 angular.module('JIRA.Teamboard')
     .controller('helpController', ['$scope', '$location', function ($scope, $location) {
         $scope.baseUrl = $location.protocol() + '://' + $location.host() + ($location.port() === 80 ? '' : ':' + $location.port());
