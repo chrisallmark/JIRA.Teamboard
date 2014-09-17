c2a8a819c4e8fd157d96433ae95ee709f85fb6e8
Chris Allmark <chris@allmarkconsulting.com>
2014-09-17 05:59:38 +0100

    Added decrement for remaining days

--- app/js/controllers/timerController.js
+++ app/js/controllers/timerController.js
@@ -17,33 +17,39 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 angular.module('JIRA.Teamboard')
-    .controller('timerController', ['$scope', '$interval', '$route', '$timeout', 'apiService', function ($scope, $interval, $route, $timeout, apiService) {
+    .controller('timerController', ['$scope', '$interval', '$timeout', 'apiService', function ($scope, $interval, $timeout, apiService) {
         var interval, timeout;
-        (function load() {
-            apiService.timer.get({
-                board: $scope.teamboard.board,
-                sprint: $scope.teamboard.sprint
-            }).$promise.then(function (timer) {
-                timer.start = moment.utc(timer.start);
-                timer.end = moment.utc(timer.end);
-                if (angular.isDefined(interval)) {
-                    $interval.cancel(interval);
+        apiService.timer.get({
+            board: $scope.teamboard.board,
+            sprint: $scope.teamboard.sprint
+        }).$promise.then(function (timer) {
+            timer.start = moment.utc(timer.start);
+            timer.end = moment.utc(timer.end);
+            if (angular.isDefined(interval)) {
+                $interval.cancel(interval);
+            }
+            interval = $interval(function () {
+                if (timer.end.isBefore(moment.utc())) {
+                    timer.countdown = '00:00:00:00';
+                } else {
+                    var now = moment.utc().add(moment().zone() * -1, 'minutes');
+                    var days = ('00' + timer.end.diff(now, 'days')).substr(-2);
+                    var hours = ('00' + timer.end.diff(now, 'hours') % 24).substr(-2);
+                    var minutes = ('00' + timer.end.diff(now, 'minutes') % 60).substr(-2);
+                    var seconds = ('00' + timer.end.diff(now, 'seconds') % 60).substr(-2);
+                    timer.countdown = days + ':' + hours + ':' + minutes + ':' + seconds;
                 }
-                interval = $interval(function () {
-                    if (timer.end.isBefore(moment.utc())) {
-                        timer.countdown = '00:00:00:00';
-                    } else {
-                        var now = moment.utc().add(moment().zone() * -1, 'minutes');
-                        var days = ('00' + timer.end.diff(now, 'days')).substr(-2);
-                        var hours = ('00' + timer.end.diff(now, 'hours') % 24).substr(-2);
-                        var minutes = ('00' + timer.end.diff(now, 'minutes') % 60).substr(-2);
-                        var seconds = ('00' + timer.end.diff(now, 'seconds') % 60).substr(-2);
-                        timer.countdown = days + ':' + hours + ':' + minutes + ':' + seconds;
-                    }
-                }, 1000);
-                $scope.timer = timer;
-            });
-            timeout = $timeout($route.reload, moment().add(1, 'day').startOf('day').diff(moment()));
+            }, 1000);
+            $scope.timer = timer;
+        });
+        (function load() {
+            timeout = $timeout(function() {
+                var now = moment();
+                if ($scope.timer.remainingDays > 0  && now.isoWeekday() !== 7 && now.isoWeekday() !== 0) {
+                    $scope.timer.remainingDays--;
+                }
+                load();
+            }, moment().add(1, 'day').startOf('day').diff(moment()));
         })();
         $scope.$on('$destroy', function (event) {
             if (angular.isDefined(interval)) {
